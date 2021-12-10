a45e505b976487b92948586e6ef2f391c4279946
ArielAvshalom <42783606+ArielAvshalom@users.noreply.github.com>
2021-12-10 10:11:42 -0500

    Control widgets 1 submit button

---
+++ Control_widgets/ui.R
@@ -0,0 +1,16 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  tags$head(tags$script(src = "message-handler.js")),
+  actionButton("do", "Click Me")
+)
